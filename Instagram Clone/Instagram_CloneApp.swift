dd957787d46184658e6f8509e54343f36c0aa391
ArpanBhowmik <bhowmik.arpan268@gmail.com>
2024-04-05 23:00:21 +0600

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Arpan Bhowmik on 31/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
